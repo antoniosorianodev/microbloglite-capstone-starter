ce1e957624cdcb80438be8751d59c7bd24e9dbcb
Antonio <asoriano@appdev.yearup.org>
2024-06-24 12:45:56 -0500

    fetch posts and display them

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,28 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+window.onload = () => {
+    displayPosts();
+}
+
+async function displayPosts() {
+    const token = parseToken();
+
+    let response = await fetch(`${apiBaseURL}/api/posts`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+
+    let data = await response.json();
+
+    document.write(JSON.stringify(data));
+}
+
+function parseToken() {
+    const loginData = localStorage.getItem("login-data");
+    const loginDataAsObject = JSON.parse(loginData);
+
+    return loginDataAsObject.token
+}
