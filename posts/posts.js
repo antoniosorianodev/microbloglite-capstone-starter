f0a38633ca06052316f4df4be44e3b66b8f7be3f
Antonio <asoriano@appdev.yearup.org>
2024-06-26 16:01:33 -0500

    add delete post functionality and icon

--- posts/posts.js
+++ posts/posts.js
@@ -68,6 +68,7 @@ async function displayPosts(dropdown, postsSection) {
 
         let likedState = "unliked";
         let likeId;
+        let deletePostButton = "";
 
         post.likes.forEach((userObject) => {
             if (userObject.username === loginData.username) {
@@ -76,18 +77,32 @@ async function displayPosts(dropdown, postsSection) {
             }
         })
 
+        if (post.username === loginData.username) {
+            deletePostButton = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash deleteButton" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                </svg>
+                `;
+        }
+
         newPost.innerHTML = `
             <div class="card mb-3 p-3" data-value="${post._id}">
             <div class="d-flex flex-row justify-content-between">
                 <div class="pb-3"><b><i>@${post.username}</i></b></div>
-                <div>${(new Date(post.createdAt)).toLocaleString()}</div>
+                <div>${deletePostButton}</div>
             </div>
-                <div class="pb-3">${post.text}</div>
+                <div class="pb-3">
+                    <div class="pb-3">${post.text}</div>
+                </div>
+                <div class="d-flex flex-row justify-content-between">
                 <div>
                     <svg data-value="${likeId}"xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill ${likedState}" id="likeButton${idx}" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
                     </svg> <span id="likeCounter${idx}">${post.likes.length}</span>
-                </div>
+                    </div>
+
+                    <div class="pb-1">${(new Date(post.createdAt)).toLocaleString()}</div>
+                    </div>
             </div>
         `;
         postsSection.appendChild(newPost);
@@ -119,6 +134,22 @@ async function displayPosts(dropdown, postsSection) {
             console.log("After", currentLikeButton);
         });
     });
+
+    let deleteButtons = document.querySelectorAll(".deleteButton");
+
+    deleteButtons.forEach((deleteButton) => {
+        deleteButton.addEventListener('click', (event) => {
+            const currentdeleteButton = event.currentTarget;
+
+            // there is no way this is good, functional? yes. good? no.
+            let currentPost = currentdeleteButton.parentElement.parentElement.parentElement
+            let currentPostId = currentPost.dataset.value;
+
+            deletePost(currentPostId);
+            console.log("After", currentdeleteButton);
+        });
+    });
+
 
 }
 
@@ -163,4 +194,17 @@ async function unlikePost(likeButton, postId) {
     console.log(data);
 
     likeButton.nextElementSibling.innerHTML = `${Number(likeButton.nextElementSibling.innerHTML) - 1}`;
+}
+
+async function deletePost(postId) {
+    const loginData = getLoginData();
+    console.log("I'm in here")
+    let response = await fetch(`${apiBaseURL}/api/posts/${postId}`, {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+        }
+    });
+    let data = await response.json();
+    console.log(data);
 }
