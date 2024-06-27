bc281c5a443ab724059c9ca59005d9eb16bace00
Antonio <asoriano@appdev.yearup.org>
2024-06-27 11:35:07 -0500

    add working redirect when logo is clicked, change colors for likes, refactor and comment posts.js

--- posts/posts.js
+++ posts/posts.js
@@ -3,27 +3,34 @@
 "use strict";
 
 window.onload = () => {
-    const sortDDL = document.querySelector("#sortDDL");
+    // these lines get the login data and save the username and token properties from it respectively,
+    // this is so that we only do this logic on page load and can pass down the values where needed, without recalculating
+    const loginData = getLoginData();
+    const username = loginData.username;
+    const token = loginData.token;
+
+    // navbar element
     const logoutButton = document.querySelector("#logoutButton");
+
+    // posts section elements
+    const sortDDL = document.querySelector("#sortDDL");
     const postsSection = document.querySelector("#postsSection");
 
-    displayPosts(sortDDL, postsSection);
+    displayPosts(sortDDL, postsSection, token, username);
 
     logoutButton.addEventListener("click", logout);
-    sortDDL.addEventListener("change", (event) => displayPosts(event.target, postsSection));
+    sortDDL.addEventListener("change", (event) => displayPosts(event.target, postsSection, token, username));
 }
 
-async function getPosts() {
-    const loginData = getLoginData();
-
+async function getPosts(token) {
     try {
-        let response = await fetch(`${apiBaseURL}/api/posts`, {
+        const response = await fetch(`${apiBaseURL}/api/posts`, {
             headers: {
-                Authorization: `Bearer ${loginData.token}`
+                Authorization: `Bearer ${token}`
             }
         });
 
-        let posts = await response.json();
+        const posts = await response.json();
 
         return posts;
     } catch (error) {
@@ -31,9 +38,10 @@ async function getPosts() {
     }
 }
 
-async function displayPosts(dropdown, postsSection) {
+async function displayPosts(dropdown, postsSection, token, username) {
+
     // this also serves as a refresh since it's a new api call, maybe do on page load and never again? idk
-    let posts = await getPosts();
+    let posts = await getPosts(token);
 
     // sort functionality for displaying posts
     switch (dropdown.value) {
@@ -53,42 +61,45 @@ async function displayPosts(dropdown, postsSection) {
             break;
         default:
             // everything else falls in here, even the "recent" case
-            // that's good because it automatically comes sorted by most recent from api, no need to 
-            console.log("how did I get here?");
+            // that's good because the data is already sorted by most recent when the api call is made
             break;
     }
 
     // empty the posts section before adding more
     postsSection.innerHTML = "";
 
-    const loginData = getLoginData();
+    const trashCanIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash deleteButton" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+        </svg>
+        `;
 
     posts.forEach((post, idx) => {
         const newPost = document.createElement("div");
 
+        // these are decleared outside of their respective loops/if conditions so that they are in scope for the construction,
+        // must use "let" instead of "const" because values can change
         let likedState = "unliked";
-        let likeId;
         let deletePostButton = "";
+        let likeId;
 
-        post.likes.forEach((userObject) => {
-            if (userObject.username === loginData.username) {
+        // changed from a forEach to for loop to stop the loop early if needed
+        for (let i = 0; i < post.likes.length; i++) {
+            if (post.likes[i].username === username) {
                 likedState = "liked";
-                likeId = userObject._id;
+                likeId = post.likes[i]._id;
+                break;
             }
-        })
+        }
 
-        if (post.username === loginData.username) {
-            deletePostButton = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash deleteButton" viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                </svg>
-                `;
+        if (post.username === username) {
+            deletePostButton = trashCanIcon;
         }
 
         newPost.innerHTML = `
-            <div class="card mb-3 p-3" data-value="${post._id}">
+            <div class="card mb-3 p-3 shadow-sm" data-value="${post._id}">
             <div class="d-flex flex-row justify-content-between">
-                <div class="pb-3"><b><i>@${post.username}</i></b></div>
+                <div class="pb-3"><b><i>${post.username}</i></b></div>
                 <div>${deletePostButton}</div>
             </div>
                 <div class="pb-3">
@@ -108,30 +119,26 @@ async function displayPosts(dropdown, postsSection) {
         postsSection.appendChild(newPost);
     });
 
-    let likeButtons = document.querySelectorAll("[id*='likeButton']");
+    const likeButtons = document.querySelectorAll("[id*='likeButton']");
 
     likeButtons.forEach((likeButton) => {
         likeButton.addEventListener('click', (event) => {
             const currentLikeButton = event.currentTarget;
 
             // there is no way this is good, functional? yes. good? no.
-            let currentPost = currentLikeButton.parentElement.parentElement;
-            let currentPostId = currentPost.dataset.value;
+            const currentPost = currentLikeButton.parentElement.parentElement.parentElement;
+            const currentPostId = currentPost.dataset.value;
 
-            // this can be .toggle()
+            // check if likePost or unlikePost should be used
             if (currentLikeButton.classList.contains("unliked")) {
-                currentLikeButton.classList.remove("unliked");
-                currentLikeButton.classList.add("liked");
-
-                likePost(currentLikeButton, currentPostId);
+                likePost(currentLikeButton, currentPostId, token);
             } else {
-                currentLikeButton.classList.add("unliked");
-                currentLikeButton.classList.remove("liked");
-
-                unlikePost(currentLikeButton, currentPostId);
+                unlikePost(currentLikeButton, currentPostId, token);
             }
 
-            console.log("After", currentLikeButton);
+            // now we can change the classes
+            currentLikeButton.classList.toggle("unliked");
+            currentLikeButton.classList.toggle("liked");
         });
     });
 
@@ -142,69 +149,87 @@ async function displayPosts(dropdown, postsSection) {
             const currentdeleteButton = event.currentTarget;
 
             // there is no way this is good, functional? yes. good? no.
-            let currentPost = currentdeleteButton.parentElement.parentElement.parentElement
-            let currentPostId = currentPost.dataset.value;
+            const currentPost = currentdeleteButton.parentElement.parentElement.parentElement
+            const currentPostId = currentPost.dataset.value;
 
-            deletePost(currentPostId);
-            console.log("After", currentdeleteButton);
+            deletePost(currentPost, currentPostId, token);
         });
     });
 
 
 }
 
-async function likePost(likeButton, postId) {
-    const loginData = getLoginData();
+async function likePost(likeButton, postId, token) {
+    try {
+        let response = await fetch(`${apiBaseURL}/api/likes`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                postId: `${postId}`
+            })
+        });
+        let data = await response.json();
 
-    let response = await fetch(`${apiBaseURL}/api/likes`, {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-            Authorization: `Bearer ${loginData.token}`,
-        },
-        body: JSON.stringify({
-            postId: `${postId}`
-        })
-    });
-    let data = await response.json();
+        console.log(data);
 
-    console.log(data);
+        // move these into a response.ok condition so we don't change things when the api call fails,
+        // maintains the illusion that it's updating real-time
+        if (response.ok) {
+            likeButton.dataset.value = data._id;
 
-    likeButton.dataset.value = data._id;
-
-    likeButton.nextElementSibling.innerHTML = `${Number(likeButton.nextElementSibling.innerHTML) + 1}`;
+            likeButton.nextElementSibling.innerHTML = `${Number(likeButton.nextElementSibling.innerHTML) + 1}`;
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-async function unlikePost(likeButton, postId) {
-    const loginData = getLoginData();
-
+async function unlikePost(likeButton, postId, token) {
     const likeId = likeButton.dataset.value;
 
-    let response = await fetch(`${apiBaseURL}/api/likes/${likeId}`, {
-        method: "DELETE",
-        headers: {
-            Authorization: `Bearer ${loginData.token}`,
-        },
-        body: JSON.stringify({
-            postId: `${postId}`
-        })
-    });
-    let data = await response.json();
+    try {
+        const response = await fetch(`${apiBaseURL}/api/likes/${likeId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                postId: `${postId}`
+            })
+        });
+        const data = await response.json();
 
-    console.log(data);
+        console.log(data);
 
-    likeButton.nextElementSibling.innerHTML = `${Number(likeButton.nextElementSibling.innerHTML) - 1}`;
+        likeButton.nextElementSibling.innerHTML = `${Number(likeButton.nextElementSibling.innerHTML) - 1}`;
+    } catch (error) {
+        console.log(error);
+    }
+
 }
 
-async function deletePost(postId) {
-    const loginData = getLoginData();
-    console.log("I'm in here")
-    let response = await fetch(`${apiBaseURL}/api/posts/${postId}`, {
-        method: "DELETE",
-        headers: {
-            Authorization: `Bearer ${loginData.token}`,
+async function deletePost(post, postId, token) {
+    try {
+        const response = await fetch(`${apiBaseURL}/api/posts/${postId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            }
+        });
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("Post has been successfully deleted. Yipeee!");
+
+            // this gets the post, then moves to it's container div, and clears it all
+            post.parentElement.innerHTML = "";
         }
-    });
-    let data = await response.json();
-    console.log(data);
+
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
 }
